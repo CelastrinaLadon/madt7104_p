cd849da211a8f31a45ddce1164dd7913fe10d7a0
PJarb <w.pachinee@gmail.com>
2025-03-02 15:33:39 +0700

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import streamlit as st
 from auth import auth_view
 from search_party import search_party_view
 from create_party import create_party_view
+from myparties import myparties_view
 def not_implement(Excepton):
     return Exception("Not implement")
     
@@ -11,7 +12,8 @@ page = st.sidebar.selectbox("Select a Page", ["Auth","Search", "Create","My Part
 mapped = {
     "auth": auth_view,
     "search": search_party_view,
-    "create": create_party_view
+    "create": create_party_view,
+    "My Parties": myparties_view
 }
 page_clean = page.replace(" ","").strip().lower()
 mapped.get(page_clean, not_implement)()
