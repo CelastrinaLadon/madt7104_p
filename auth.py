883bde5b3f04c58d82902140af3fe6621598a814
Thotsaphon Sirikutta <33342066+CelastrinaLadon@users.noreply.github.com>
2025-03-02 14:09:19 +0700

    Update auth.py

--- auth.py
+++ auth.py
@@ -7,13 +7,20 @@ from yaml.loader import SafeLoader
 with open('./credentials.yaml') as file:
     config = yaml.load(file, Loader=SafeLoader)
     
+def load_config():
+    try:
+        with open("config.yaml") as file:
+            return yaml.load(file, Loader=SafeLoader)
+    except FileNotFoundError:
+        st.error("Configuration file not found.")
+        return None
+
 def auth_view():
     st.title("Login Page")
 
     config = load_config()
     if not config:
         return
-
     authenticator = stauth.Authenticate(
         config["credentials"],
         config["cookie"]["name"],
@@ -22,6 +29,7 @@ def auth_view():
         config["preauthorized"]
     )
 
+    # Login form
     name, authentication_status, username = authenticator.login("Login", "main")
 
     if authentication_status:
@@ -32,4 +40,3 @@ def auth_view():
         st.warning("Please enter your credentials")
 
     return authentication_status
-
