cb4cb70505682b0df22b8014ea4f3c055cbb0103
Thotsaphon Sirikutta <33342066+CelastrinaLadon@users.noreply.github.com>
2025-03-02 15:35:31 +0700

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 from auth import auth_view
 from search_party import search_party_view
 from create_party import create_party_view
-from myparties import myparties_view
+from my_parties import my_parties_view
 def not_implement(Excepton):
     return Exception("Not implement")
     
@@ -13,7 +13,7 @@ mapped = {
     "auth": auth_view,
     "search": search_party_view,
     "create": create_party_view,
-    "My Parties": myparties_view
+    "my_parties": my_parties
 }
 page_clean = page.replace(" ","").strip().lower()
 mapped.get(page_clean, not_implement)()
