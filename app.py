4ec8b6edc5cdfa42a2b2aba0be8b183f2da68586
Thotsaphon Sirikutta <33342066+CelastrinaLadon@users.noreply.github.com>
2025-03-02 13:58:52 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,18 @@
 import streamlit as st
+from auth import auth_page
+from main import main_page
+from search import search_page
 
-def main_page():
-    st.title("Main Page")
-    st.write("Welcome to the main page")
+def not_implement(Excepton):
+    return Exception("Not implement")
+    
+# Sidebar navigation
+page = st.sidebar.selectbox("Select a Page", ["Auth","Create","My Parties"])
+
+mapped = {
+    "auth": not_implement(),
+    "search": not_implement(),
+    "create": not_implement()
+}
+mapped.get(page, not_implement)()
+
